25cba33e56df1b35a5e55f2167a5e6916c111961
Gabriel Takaki <gabitj@icloud.com>
2021-10-29 10:48:32 -0300

    handle changes of input changes

--- src/screens/SearchScreens.tsx
+++ src/screens/SearchScreens.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import tw from 'tailwind-react-native-classnames';
 
+interface Provider {
+  success: boolean;
+  message: string;
+}
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={ term } onTermChange={ (val: string) => setTerm(val) } />
       <Text>Search Sceen</Text>
     </View>
   );
